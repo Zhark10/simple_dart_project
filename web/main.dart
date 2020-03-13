02ce0c2b2e6502ac8672b4440800054a8a9ba4fe
Zhark10 <arkady.zharavin@citronium.biz>
2020-03-13 23:14:31 +0300

    minor fix (changed link for script)

--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,5 @@
 import 'dart:html';
 
-main() {
- String text = "Hello dart for web";
- Element h1  = querySelector('h1');
- h1.text = text;
+void main() {
+  querySelector('#output').text = 'Your Dart app is running.';
 }
